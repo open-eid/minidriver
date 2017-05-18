9e89ff7a3080f2432bde31455affd89321399461
Raul Metsma <raul@metsma.ee>
2017-05-18 16:53:24 +0300

    Cleanup and improve logging
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- precompiled.h
+++ precompiled.h
@@ -35,7 +35,8 @@
 #include <Commctrl.h>
 #include <Shellapi.h>
 #include "cardmod.h"
- 
+#include <algorithm>
+
 // common controls
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
