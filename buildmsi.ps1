a69dea331d6d773aa099d945485531b5a5039b4a
Raul Metsma <raul@metsma.ee>
2017-09-22 13:57:49 +0300

    Package new drivers
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- buildmsi.ps1
+++ buildmsi.ps1
@@ -2,30 +2,28 @@
 param(
    [string]$candle = "$env:WIX\bin\candle.exe",
    [string]$light = "$env:WIX\bin\light.exe",
-   [string]$msiversion = "3.11.0.1175",
+   [string]$msiversion = "3.12.0.77",
    [string]$sign = $null
 )
 
 $cwd = Split-Path $MyInvocation.MyCommand.Path
 $shell = new-object -com shell.application
+mkdir -Force -Path "minidriver\W10"
+mkdir -Force -Path "minidriver\PreW10"
+foreach($item in $shell.NameSpace("$cwd\esteidcm_$($msiversion)-W10.zip").items()) {
+    $shell.Namespace("$cwd\minidriver\W10").CopyHere($item,0x14)
+}
+foreach($item in $shell.NameSpace("$cwd\esteidcm_$($msiversion)-PreW10.zip").items()) {
+    $shell.Namespace("$cwd\minidriver\PreW10").CopyHere($item,0x14)
+}
 foreach($platform in @("x86", "x64")) {
-    foreach($version in @("7", "8", "8.1")) {
-        mkdir -Force -Path "minidriver\$version$platform"
-        foreach($item in $shell.NameSpace("$cwd\esteidcm.$($msiversion)_win$version$platform.zip").items()) {
-            $shell.Namespace("$cwd\minidriver\$version$platform").CopyHere($item,0x14)
-        }
-    }
-    foreach($version in @("10")) {
-        mkdir -Force -Path "minidriver\$version$platform"
-        & expand.exe .\esteidcm.$($msiversion)_win$version$platform.cab -F:* "minidriver\$version$platform"
-    }
     & $candle -nologo minidriver.wxs "-dPlatform=$platform" "-dMSI_VERSION=$msiversion" `
         -arch $platform -ext WixDifxAppExtension
-    & $light -nologo -out "minidriver_$msiversion.$platform.msi" -ext WixUIExtension -ext WixDifxAppExtension `
+    & $light -nologo -out "minidriver-$msiversion.$platform.msi" -ext WixUIExtension -ext WixDifxAppExtension `
         "-dWixUILicenseRtf=LICENSE.rtf" "-dWixUIDialogBmp=dlgbmp.bmp" "-dWixUIBannerBmp=banner.bmp" `
         "$env:WIX\bin\difxapp_$platform.wixlib" minidriver.wixobj
     if($sign) {
         & signtool.exe sign /a /v /s MY /n "$sign" /fd SHA256 /du http://installer.id.ee `
-            /t http://timestamp.verisign.com/scripts/timstamp.dll "minidriver_$msiversion.$platform.msi"
+            /t http://timestamp.verisign.com/scripts/timstamp.dll "minidriver-$msiversion.$platform.msi"
     }
 }
