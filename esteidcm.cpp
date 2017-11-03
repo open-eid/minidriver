42b379ff3c9a49cd69a1a6a525a1fd6e3f82a1a1
Raul Metsma <raul@metsma.ee>
2017-11-03 15:23:30 +0200

    Minor changes - Cleanup old registry keys on update - Simplify folder structure - Enable precompiled headers - Include debug symbols in zip
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- esteidcm.cpp
+++ esteidcm.cpp
@@ -10,7 +10,9 @@
 *
 */
 
-#include "precompiled.h"
+#include "stdafx.h"
+
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #define AUTH_PIN_ID ROLE_USER
 #define SIGN_PIN_ID 3
---
+++ stdafx.cpp
@@ -0,0 +1,13 @@
+/*
+* EstEID Minidriver
+* 
+* This software is released under either the GNU Library General Public
+* License (see LICENSE.LGPL) or the BSD License (see LICENSE.BSD).
+* 
+* Note that the only valid version of the LGPL license as far as this
+* project is concerned is the original GNU Library General Public License
+* Version 2.1, February 1999
+*
+*/
+
+#include "stdafx.h"
